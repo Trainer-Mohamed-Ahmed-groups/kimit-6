8641ee90d816b6acca4d278304827b47dae17c07
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2023-08-25 19:42:29 +0300

    Session 1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import reactLogo from "./logo.svg";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <input type="text" />
+        <img src={reactLogo} alt="This is logo" />
+        Hello first app in react
+      </div>
+      <div className="test"></div>
+    </>
+  );
+}
+
+export default App;
